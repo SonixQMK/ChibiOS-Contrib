017a587fe4753bcffa68aee015d5f19df1d492b3
HorrorTroll <sonicvipduc@gmail.com>
2022-04-02 20:49:01 +0700

    240 flash: comply with the hardware

--- os/hal/ports/SN32/LLD/SN32F24x/FLASH/Flash.h
+++ os/hal/ports/SN32/LLD/SN32F24x/FLASH/Flash.h
@@ -41,7 +41,7 @@ typedef enum { FLASH_FAIL, FLASH_OKAY} FLASH_Status;
 
 FLASH_Status FLASH_EraseSector (uint32_t);
 FLASH_Status FLASH_ProgramPage (uint32_t, uint32_t, uint16_t);
-FLASH_Status FLASH_ProgramHalfWord(uint32_t, uint16_t);
+FLASH_Status FLASH_ProgramWord(uint32_t, uint16_t);
 
 
 #endif	/* __SN32F240_FLASH_H */
